69c562122c751bcf1aef151dc9e836eca328852f
Sarah Betack <email": null>
2017-05-08 15:07:00 -0500

    complete categories index and show, items index, show, edit, delete, new

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,23 @@
 class ItemsController < ApplicationController
   before_action :find_item_and_category, only: [:edit, :update, :show, :destroy]
 
+  def new
+    @category = Category.find(params[:category_id])
+    @item = Item.new
+  end
+
+  def create
+    p params
+    @category = Category.find(params[:category_id])
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to category_item_path
+    else
+      @errors = @item.errors.full_messages[0..-2]
+      render :new
+    end
+  end
+
   def show
   end
 
@@ -29,7 +46,7 @@ class ItemsController < ApplicationController
 
   def find_item_and_category
     @category = Category.find(params[:category_id])
-    @item = Item.find(params[:id])
+    @item = Item.find_by(params[:id])
   end
 
   def item_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get '/' => 'categories#index'
   get '/categories/:category_id' => 'categories#show', as: 'category'
+  get '/categories/:category_id/items/new' => 'items#new'
   get '/categories/:category_id/items/:id' => 'items#show', as: 'item'
 
   resources :categories do
