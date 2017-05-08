fa9b323e2260d2d9edc8d089ae4ba3be843681e5
Sarah Betack <email": null>
2017-05-08 10:55:45 -0500

    associate categories and items correctly

--- db/migrate/20170508151912_create_items.rb
+++ db/migrate/20170508151912_create_items.rb
@@ -1,9 +1,10 @@
-class CreatePosts < ActiveRecord::Migration[5.0]
+class CreateItems < ActiveRecord::Migration[5.0]
   def change
-    create_table :posts do |t|
+    create_table :items do |t|
       t.string :name, null: false
       t.integer :price, null: false
       t.string :description, null: false
+      t.integer :category_id
 
       t.timestamps
     end
